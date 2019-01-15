96f43d67deb890575557b82a43172ab1b02f8f2c
zhangsan <zhangsan@itcast.cn>
2019-01-15 14:16:23 +0800

    提交到gitfirst

---
+++ git-test-project/src/main/java/demo.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Administrator
+ * @CreateTime: 2019-01-15 14:15
+ * @Description: ${Description}
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+}
